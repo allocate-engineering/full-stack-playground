81e2096aa06580185964266b29a17cb5c0631d82
Jeremy Caron <jeremy@allocate.co>
2023-07-12 14:50:58 -0400

    Add the transactions.csv file for import

--- Allocate.Web.DemoApi/DataModels/Security.cs
+++ Allocate.Web.DemoApi/DataModels/Security.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Allocate.Common.Database.DataModels;
 
 namespace Allocate.Web.DemoApi.DataModels
